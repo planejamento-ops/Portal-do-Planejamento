175286a9faef3123a415f4029b4583ec4cafa79a
planejamento-ops <Julia.De.Almeida.Anutnes@cevalogistics.com>
2026-09-16 17:49:24 -0300

    📈 [AUTO-SYNC] Demandas WMS Atualizado - 16/09 17:49

---
+++ dados_demandas.js
@@ -0,0 +1,44 @@
+window.dadosDemandaWMS = {
+  "atualizado_em": "16/09/2026 17:49",
+  "onda_atual": "SPI Varejo",
+  "data_onda": "16/09/2026, 01:12:34",
+  "real_pcs": 18824,
+  "real_cxs": 944,
+  "previa_pcs": 38712,
+  "acuracia_pct": 49,
+  "top_estacoes": [
+    {
+      "estacao": "07",
+      "volume": 7302
+    },
+    {
+      "estacao": "34",
+      "volume": 1826
+    },
+    {
+      "estacao": "31",
+      "volume": 524
+    }
+  ],
+  "top_familias": [
+    {
+      "familia": "Cabelos",
+      "pct": 23
+    },
+    {
+      "familia": "SuporteVenda",
+      "pct": 22
+    },
+    {
+      "familia": "Perfumaria",
+      "pct": 14
+    }
+  ],
+  "predios": {
+    "GV": 2016,
+    "FR": 16808,
+    "AG": 7302,
+    "BG": 9271,
+    "PC": 235
+  }
+};
