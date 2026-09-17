975937d566355fe9d4744b45843b21a85c792d02
planejamento-ops <Julia.De.Almeida.Anutnes@cevalogistics.com>
2026-09-17 12:31:50 -0300

    📈 [AUTO-SYNC] WMS Atualizado - 17/09 12:31

--- dados_demandas.js
+++ dados_demandas.js
@@ -1,64 +1,63 @@
 window.dadosDemandaWMS = {
-  "atualizado_em": "17/09/2026 11:49",
+  "atualizado_em": "17/09/2026 12:31",
+  "periodo": "13/09 a 19/09",
   "ondas": [
     {
-      "onda_atual": "SPI Varejo",
-      "data_onda": "16/09/2026, 01:12:34",
-      "real_cxs": 944,
-      "real_pcs": 18824,
-      "previa_cxs": 2036,
-      "acuracia_cxs": 46,
-      "perfil_est_cx": 6.4,
-      "perfil_skus_cx": 9.5,
-      "perfil_pcs_cx": 19.0,
+      "onda_atual": "VAREJO",
+      "real_cxs": 6226,
+      "real_pcs": 100716,
+      "previa_cxs": 0,
+      "acuracia_cxs": 0,
+      "perfil_est_cx": 6.3,
+      "perfil_skus_cx": 8.9,
+      "perfil_pcs_cx": 17.9,
       "top_est_cxs": [
         {
           "estacao": "07",
-          "volume": 828
+          "volume": 4256
         },
         {
-          "estacao": "34",
-          "volume": 405
+          "estacao": "33",
+          "volume": 1537
         },
         {
           "estacao": "21",
-          "volume": 293
+          "volume": 1418
         },
         {
           "estacao": "31",
-          "volume": 269
+          "volume": 1316
         },
         {
           "estacao": "22",
-          "volume": 233
+          "volume": 1199
         }
       ],
       "top_est_pcs": [
         {
           "estacao": "07",
-          "volume": 7302
+          "volume": 33360
+        },
+        {
+          "estacao": "33",
+          "volume": 6147
+        },
+        {
+          "estacao": "36",
+          "volume": 2917
         },
         {
           "estacao": "34",
-          "volume": 1826
+          "volume": 2873
         },
         {
           "estacao": "31",
-          "volume": 524
-        },
-        {
-          "estacao": "29",
-          "volume": 481
-        },
-        {
-          "estacao": "27",
-          "volume": 469
+          "volume": 2555
         }
       ]
     },
     {
-      "onda_atual": "SPI Carteira",
-      "data_onda": "16/09/2026, 00:07:47",
+      "onda_atual": "SPI",
       "real_cxs": 15702,
       "real_pcs": 729469,
       "previa_cxs": 20486,
@@ -112,8 +111,7 @@ window.dadosDemandaWMS = {
       ]
     },
     {
-      "onda_atual": "MGC Carteira",
-      "data_onda": "14/09/2026, 21:08:22",
+      "onda_atual": "MGC",
       "real_cxs": 2387,
       "real_pcs": 116954,
       "previa_cxs": 2307,
@@ -163,116 +161,6 @@ window.dadosDemandaWMS = {
         {
           "estacao": "16",
           "volume": 2487
-        }
-      ]
-    },
-    {
-      "onda_atual": "MGI Carteira",
-      "data_onda": "11/09/2026, 20:38:40",
-      "real_cxs": 10889,
-      "real_pcs": 517838,
-      "previa_cxs": 12208,
-      "acuracia_cxs": 89,
-      "perfil_est_cx": 1.9,
-      "perfil_skus_cx": 9.0,
-      "perfil_pcs_cx": 50.5,
-      "top_est_cxs": [
-        {
-          "estacao": "04",
-          "volume": 988
-        },
-        {
-          "estacao": "07",
-          "volume": 981
-        },
-        {
-          "estacao": "23",
-          "volume": 592
-        },
-        {
-          "estacao": "32",
-          "volume": 592
-        },
-        {
-          "estacao": "25",
-          "volume": 589
-        }
-      ],
-      "top_est_pcs": [
-        {
-          "estacao": "04",
-          "volume": 57513
-        },
-        {
-          "estacao": "07",
-          "volume": 28975
-        },
-        {
-          "estacao": "34",
-          "volume": 13442
-        },
-        {
-          "estacao": "14",
-          "volume": 12366
-        },
-        {
-          "estacao": "25",
-          "volume": 11734
-        }
-      ]
-    },
-    {
-      "onda_atual": "SPC Varejo",
-      "data_onda": "11/09/2026, 00:56:29",
-      "real_cxs": 1892,
-      "real_pcs": 31006,
-      "previa_cxs": 0,
-      "acuracia_cxs": 0,
-      "perfil_est_cx": 6.8,
-      "perfil_skus_cx": 8.9,
-      "perfil_pcs_cx": 17.5,
-      "top_est_cxs": [
-        {
-          "estacao": "07",
-          "volume": 1458
-        },
-        {
-          "estacao": "23",
-          "volume": 885
-        },
-        {
-          "estacao": "31",
-          "volume": 491
-        },
-        {
-          "estacao": "26",
-          "volume": 462
-        },
-        {
-          "estacao": "PC",
-          "volume": 444
-        }
-      ],
-      "top_est_pcs": [
-        {
-          "estacao": "07",
-          "volume": 7864
-        },
-        {
-          "estacao": "32",
-          "volume": 2259
-        },
-        {
-          "estacao": "37",
-          "volume": 2197
-        },
-        {
-          "estacao": "33",
-          "volume": 1898
-        },
-        {
-          "estacao": "31",
-          "volume": 1003
         }
       ]
     }
