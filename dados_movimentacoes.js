1f149f7f9daae3192c2d975d6859927b49e9044f
planejamento-ops <Julia.De.Almeida.Anutnes@cevalogistics.com>
2026-09-25 09:08:05 -0300

    Add files via upload

--- dados_movimentacoes.js
+++ dados_movimentacoes.js
@@ -1,5 +1,5 @@
 window.dadosMovimentacoes = {
-  "atualizado_em": "25/09/2026 09:00",
+  "atualizado_em": "25/09/2026 08:54",
   "total_dia": 0,
   "pendentes_hoje": 0,
   "pendentes_antigas": 2,
--- dados_tu.js
+++ dados_tu.js
@@ -1,5 +1,5 @@
 window.dadosDashboard = {
-  "atualizado_em": "25/09/2026 08:58",
+  "atualizado_em": "25/09/2026 08:53",
   "total_tus": 72,
   "caixas_total_mes": 335848,
   "pecas_total_mes": 11696390,
